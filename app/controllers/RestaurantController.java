eea09c2473b64b162f8c93f107984050d4fb7e5b
Vash <vash000@gmail.com>
2016-06-12 21:57:37 +0200

    Add cache for id requests

--- app/controllers/RestaurantController.java
+++ app/controllers/RestaurantController.java
@@ -4,6 +4,7 @@ import controllers.actions.VersionedAction;
 import models.Restaurant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import play.cache.CacheApi;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.libs.Json;
@@ -11,6 +12,7 @@ import play.mvc.With;
 import services.RestaurantService;
 
 import javax.inject.Inject;
+import java.time.Duration;
 import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 
@@ -22,10 +24,11 @@ public final class RestaurantController extends Controller {
 
     private final static Logger log = LoggerFactory.getLogger(RestaurantController.class);
     private final RestaurantService service;
-
+    private final CacheApi cache;
     @Inject
-    public RestaurantController(RestaurantService service) {
+    public RestaurantController(RestaurantService service, CacheApi cache) {
         this.service = service;
+        this.cache = cache;
     }
 
     public CompletionStage<Result> list() {
@@ -37,19 +40,32 @@ public final class RestaurantController extends Controller {
      * @param id Id of the restaurant to edit
      */
     public Result getById(Integer id) {
-
         log.info("Requested by id: " + String.valueOf(id));
-        final Optional<Restaurant> restaurant = service.findById(id);
+
+        final Optional<Restaurant> restaurant = fetchById(id);
 
         final Result result;
         if(restaurant.isPresent()) {
-            result = okJson(restaurant.get());
+            final Restaurant fresh = restaurant.get();
+            cache.set("restaurant."+ fresh.id,fresh, 5);
+            result = okJson(fresh);
             log.info("Response ok id: " + String.valueOf(id));
         } else {
             result = notFound(); //No body
             log.info("Response notfound " + String.valueOf(id));
         }
         return result;
+    }
+
+    private Optional<Restaurant> fetchById(Integer id) {
+
+        final Optional<Restaurant> opt = Optional.ofNullable(cache.get("restaurant." + String.valueOf(id)));
+
+        if(opt.isPresent()) {
+            return opt;
+        } else {
+            return service.findById(id);
+        }
     }
 
     private static Result okJson(Object o) {
--- build.sbt
+++ build.sbt
@@ -7,8 +7,7 @@ scalaVersion := "2.11.8"
 lazy val root = (project in file(".")).enablePlugins(PlayJava, PlayEbean).settings {
   libraryDependencies ++= Seq(
     jdbc,
-    // https://adrianhurt.github.io/play-bootstrap/
-    "com.adrianhurt" %% "play-bootstrap" % "1.0-P25-B3"
+    cache
   )
 }
 
